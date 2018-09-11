2aeb4ce5a7566f9f5d77fd37def834ac7d7020ee
mdmoin786313 <40501016+mdmoin786313@users.noreply.github.com>
2018-09-11 11:41:31 +0530

    adding file strcture

---
+++ signup.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+  <link href="images\icon.png" rel="icon">
+  <title>Join Us!</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body class="back_2" align="center">
+  <div class="signup_box">
+    <div class="box_1">
+      <h1 style="font-size: 40px;">Join Us Now!</h1>
+      <div class="underline_login" align="center"></div><br><br>
+      <form method="post">
+        <label class="label_1">Full Name</label><br>
+        <input type="text" name="fname" placeholder="Enter Your Full Name Here..." class="input_2" required><br><br>
+        <label class="label_1">Email ID</label><br>
+        <input type="text" name="email" placeholder="Enter Your Email ID Here..." class="input_2" required><br><br>
+        <label class="label_1">Mobile Number</label><br>
+        <input type="text" name="mobile" placeholder="Enter Your Mobile Number Here..." class="input_2" required><br><br>
+        <label class="label_1">Password</label><br>
+        <input type="password" name="password" placeholder="Enter Your Password Here..." class="input_2" required><br><br>
+        <label class="label_1">Re-Enter Password</label><br>
+        <input type="password" name="rpassword" placeholder="Re-Enter Your Password Here..." class="input_2" required><br><br>
+        <input type="submit" name="join" value="JOIN" class="signup_button">
+      </form>
+    </div>
+    <div>
+      <br><br><br><br><br>
+      <h1>Earn Just By Invite!</h1>
+      <img src="images\verified.png" class="verified">
+      <h2>Join Us Now And Get &#x20B9;200 And Invite Your Friends, Relatives, Collagues, Etc. And Get Upto &#x20B9;50 Per Invitation On Their Joining!</h2>
+    </div>
+  </div>
+</body>
+</html>
